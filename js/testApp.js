7c427ccbf04960e6d5de14fe2e2d4954ed8f94f8
beeraman <anilkumar.beeram@adp.com>
2017-01-31 17:36:49 +0530

    welcome page with header state.go to new page

---
+++ js/testApp.js
@@ -0,0 +1,64 @@
+/**
+ * Created by beeraman on 1/31/2017.
+ */
+(function(angular){
+    'use strict';
+
+    var routerApp = angular.module('testApp',['ui.router']);
+        routerApp.config(function($stateProvider,$urlRouterProvider){
+            //$urlRouterProvider.otherwise('/welcome');
+            $stateProvider
+                .state('app',{
+                    url:'/main',
+                    templateUrl:'partials/main.html',
+                    abstract:true,
+                    requireLogin:true,
+                    controller:function($scope){
+
+                    }
+                })
+                .state('welcome',{
+                    url:'/welcome',
+                    requireLogin:false,
+                    views:{
+                        '':{templateUrl:"partials/welcome.html"},
+                        //'header':{templateUrl:"partials/header.html"},
+                        //the above statement dosent display header in welcome
+                        //after adding @welcome is displays
+                        'header@welcome':{
+                            templateUrl:"partials/header.html",
+                            //controller:headerController
+                            controller:function($scope,$state){
+                                $scope.click=function(){
+                                    console.log('asdfasdfsad fdas f');
+                                    $state.go('app.dash');
+                                }
+                            }
+                        },
+                        'footer@welcome':{templateUrl:"partials/footer.html"},
+                        'intro@welcome':{templateUrl:"partials/intro.html"}
+                    }
+                })
+                .state('app.dash',{
+                    url:'/dashboard',
+                    template:"dashboard sdf sdfdsf ads fs "
+                })
+        });
+
+
+}(angular));
+
+/* Displaying header, body and footer
+
+ .state('welcome',{
+ url:'/welcome',
+ views:{
+ '':{templateUrl:"partials/welcome.html"},
+ //'header':{templateUrl:"partials/header.html"},
+ //the above statement dosent display header in welcome
+ //after adding @welcome is displays
+ 'header@welcome':{templateUrl:"partials/header.html"},
+ 'footer@welcome':{templateUrl:"partials/footer.html"}
+ }
+ })
+ */
